b593ac3f11a3b86e128c451d3685cf9a4a1a6983
Ryan Murphy <rymurphy12@gmail.com>
2024-12-28 20:08:48 -0600

    Have the facade built, now to build out the plumbing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+/* import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg' */
+import './App.css';
+import axios from 'axios';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [file, setFile] = useState();
+
+  function handleChange(event) {
+    setFile(event.target.files[0]);
+  }
+  
+  function handleSubmit(event) {
+    event.preventDefault()
+    const url = 'AWS endpoint here'
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('fileName', file.Name);
+    const config = {
+      headers: {
+        'content-type': "multipart/form-data",
+      }
+    };
+    axios.post(url, formData, config).then((response) => {
+      console.log(response.data);
+    });
+  }
 
   return (
     <>
-      <div>
+    <form onSubmit={handleSubmit}>
+      <h1>STL File Upload</h1>
+      <input type="file" onChange={handleChange}/>
+      <button type="submit">Upload</button>
+    </form>
+      {/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,7 +53,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
   )
 }
